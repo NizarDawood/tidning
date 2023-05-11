6026b6c081959325e5554d9c356a1be8c3b12028
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2023-05-11 13:41:13 +0200

    idag har jag jobbat med att hämta information från api nästa lektion ska jag arbeta med att strukturen på all data frö att lägga upp det på view

--- tidning1/src/Väder.java
+++ tidning1/src/Väder.java
@@ -1,3 +1,25 @@
-public class Väder {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.net.URLConnection;
 
+public class Väder {
+    public static void main(String[] args) throws IOException {
+        URL u = new URL("http://api.temperatur.nu/tnu_1.17.php?p=ekholmen&cli=api_demo&token=XXXXXXXX");
+        URLConnection conn = u.openConnection();
+        BufferedReader in = new BufferedReader(
+                new InputStreamReader(
+                        conn.getInputStream()));
+        StringBuffer buffer = new StringBuffer();
+        String inputLine;
+        while ((inputLine = in.readLine()) != null)
+            buffer.append(inputLine);
+        in.close();
+        System.out.println(buffer.toString());
+
+
+
+    }
 }
+
